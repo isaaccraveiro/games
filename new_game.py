939638dd3825bf1184192e667e129ee3cee12dc6
Isaac Craveiro <isaac.craveiro.019@gmail.com>
2020-06-04 16:25:11 +0100

    got rid of some hard coded pieces and made barriers to the screen

--- new_game.py
+++ new_game.py
@@ -9,10 +9,11 @@ game_over = False
 player_pos = [700, 500]
 player_size = 50
 
+BACKGROUND = (169,169,169)
 COLOUR = (255,123,67)
 WIDTH = 800
 HEIGHT = 600
-
+MOVEMENT_SIZE = 50
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 while not game_over:
@@ -23,22 +24,34 @@ while not game_over:
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            print("key down")
+            print("key pressed")
             x = player_pos[0]
             y = player_pos[1]
 
             if event.key == pygame.K_LEFT:
-                print("key left")
-                x = x - player_size
+                if x > 0:
+                    x = x - MOVEMENT_SIZE
+                print("key left: ", x, y)
+
             elif event.key == pygame.K_RIGHT:
-                print("key right")
-                x = x + player_size
+                if x < WIDTH - player_size:
+                    x = x + MOVEMENT_SIZE
+                print("key right:", x, y)
 
             elif event.key == pygame.K_DOWN:
-                print("key down ")
-                x = x + player_size
+                if y < HEIGHT - player_size:
+                    y = y + MOVEMENT_SIZE
+                print("key down", x, y)
+
+            elif event.key == pygame.K_UP:
+                if y > 0:
+                    y = y - MOVEMENT_SIZE
+                print("key up ", x, y)
 
             player_pos = [x,y]
+            print('foo', flush=True)
+
+    screen.fill(BACKGROUND)
 
     pygame.draw.rect(screen, COLOUR, (player_pos[0], player_pos[1], player_size, player_size))
 
